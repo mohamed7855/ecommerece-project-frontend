58340c5c35659b309a6e06bb8d63b67d65f2a048
mohamed7855 <martm7726@gmail.com>
2024-06-18 14:16:17 +0300

    update cart status

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -22,7 +24,9 @@ export class ProductListComponent implements OnInit {
 
   previousKeyword: string ="";
 
-  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+  constructor(private productService: ProductService,
+              private cartService: CartService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(()=>{
@@ -101,5 +105,8 @@ export class ProductListComponent implements OnInit {
 
   addToCart(theProduct: Product){
     console.log(`Adding to cart ${theProduct.name}, ${theProduct.unitPrice}`);
+
+    const theCartItem = new CartItem(theProduct);
+    this.cartService.addToCart(theCartItem);
   }
 }
